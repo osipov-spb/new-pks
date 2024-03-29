d3b2cd3ca66b79f86f2921b7a9edc974e4d9484f
osipov-spb <tellan.dm@gmail.com>
2024-03-29 16:52:31 +0300

    motivation changes

--- src/сomponents/menu/motivation.jsx
+++ src/сomponents/menu/motivation.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
-import {Card, Progress, Space, Typography } from 'antd'
+import {Card, Progress, Space, Typography, Carousel, Radio} from 'antd'
+import React, { useState } from 'react';
 import Divider from "../divider";
 
 const { Text, Title } = Typography;
+
+const contentStyle = {
+    height: '200px',
+    color: 'black',
+    lineHeight: '100px',
+    textAlign: 'center',
+    background: 'white',
+    width: '200px'
+};
+
+const wrapper_style ={
+    height: '200px',
+    width: '200px'
+};
+
 class Motivation extends React.Component {
     constructor(props) {
         super(props);
         this.state = {}
     }
+
 
     componentDidMount() {
         const _plan_completion = 0
@@ -19,7 +35,8 @@ class Motivation extends React.Component {
             plan_completion: _plan_completion,
             leader: _leader,
             place: _place,
-            trend: _trend
+            trend: _trend,
+            dotPosition: 'left'
         })
 
         window.motivation_SetData = (_plan_completion, _leader, _place, _trend) => {
@@ -35,17 +52,26 @@ class Motivation extends React.Component {
 
     render() {
         return (
-            <Card align='left' bordered={false}>
-                <h4><Space size='middle'>Выполнение плана<Progress type="circle" percent={this.state.plan_completion}
-                                                                  width={45}/></Space></h4>
+            <Carousel style={contentStyle} autoplay autoplaySpeed={5000} dotPosition='left'>
+                <div class="wrapper" style={wrapper_style}>
+                    <Space size='middle'>Выполнение плана<Progress type="circle"
+                                                                   percent={this.state.plan_completion}
+                                                                       width={45}/></Space>
+                </div>
+                <div class="wrapper" style={wrapper_style}>
+                    <Space direction="horizontal"><Text>Лидер продаж: <Text
+                        type="secondary">{this.state.leader}</Text></Text></Space>
+                </div>
+                <div class="wrapper" style={wrapper_style}>
+                    <Space direction="horizontal"><Text>Ваше место: <Text
+                        type="secondary">{this.state.place}</Text></Text></Space>
+                </div>
+                <div class="wrapper" style={wrapper_style}>
+                   <Space direction="horizontal"><Text>Тенденция: <Text
+                        type="secondary">{this.state.trend}</Text></Text></Space>
+                </div>
 
-                <Space direction="vertical" size='xs'>
-                    <Space direction="horizontal"><Text>Лидер продаж: <Text type="secondary">{this.state.leader}</Text></Text></Space>
-                    <Space direction="horizontal"><Text>Ваше место: <Text type="secondary">{this.state.place}</Text></Text></Space>
-                    <Space direction="horizontal"><Text>Тенденция: <Text type="secondary">{this.state.trend}</Text></Text></Space>
-                </Space>
-
-            </Card>
+            </Carousel>
         )
     }
 }
